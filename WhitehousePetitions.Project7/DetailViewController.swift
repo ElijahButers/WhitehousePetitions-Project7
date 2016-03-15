7e8383d16b23ddac91f96f3d5d20d5746b44cde2
ElijahButers <Elijahbuters@gmail.com>
2016-03-15 20:47:57 +0200

    Added code where string called html that contains everything to show the page, and passed in to web view's loadHTMLString().

--- WhitehousePetitions.Project7/DetailViewController.swift
+++ WhitehousePetitions.Project7/DetailViewController.swift
@@ -36,6 +36,21 @@ class DetailViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
+        
+        guard detailItem != nil else { return }
+        
+        if let body = detailItem["body"] {
+            var html = "<html>"
+            html += "<head>"
+            html += "<meta name=\"viewpoint\" content=\"width=device-width,initial-scale=1\">"
+            html += "<style> body { font-size: 150%; } </style>"
+            html += "</head>"
+            html += "</body>"
+            html += body
+            html += "</body>"
+            html += "</html>"
+            webView.loadHTMLString(html, baseURL: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
